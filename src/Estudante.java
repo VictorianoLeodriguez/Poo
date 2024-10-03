682c2b8a4f5e2cf0a5c05ba02dc81c416b118e80
VictorianoLeodriguez <ev_leodriguez@hotmail.com>
2024-10-03 14:24:34 -0300

    Codigo Incompleto de poo

---
+++ src/Estudante.java
@@ -0,0 +1,20 @@
+public class Estudante{
+    public static void main (String[] args) throws Exception{
+
+        Estudante estudante1 = new Estudante();
+        estudante1.age = 12;
+        estudante1.name = "Jonh";
+        estudante1.color = Color.FAIR;
+        estudante1.Sex = Sex.MALE;
+
+        Estudante estudante2 = new Estudante();
+        estudante2.age = 10;
+        estudante2.name = "Laís";
+        estudante2.color = Color.FAIR;
+        estudante2.Sex = Sex.FEMALE;
+
+
+
+
+    }
+}
